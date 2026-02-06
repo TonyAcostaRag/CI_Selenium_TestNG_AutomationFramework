26eead0cbf6172ff9530896f35c833e91ac52d39
Tony Acosta <tony.1293@hotmail.com>
2026-02-05 22:15:18 -0600

    remove unused flags

--- src/main/java/framework/DriverFactory.java
+++ src/main/java/framework/DriverFactory.java
@@ -58,8 +58,6 @@ public class DriverFactory {
                 case "RemoteChrome":
                     ChromeOptions options = new ChromeOptions();
                     options.addArguments("--headless=new");
-                    //options.addArguments("--no-sandbox");
-                    //options.addArguments("--disable-dev-shm-usage");
                     options.addArguments("--window-size=1920,1080");
 
                     driver.set(new RemoteWebDriver(new URL(remoteUrl), options));
